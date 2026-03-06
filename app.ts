2fa42c24b0ee6e6c48ede0e9de3d980733e2428a
Punam <159411425+punammomi@users.noreply.github.com>
2026-03-07 00:17:11 +0600

    Practice with multiplication and division function and also practice object using function

--- app.ts
+++ app.ts
@@ -4,3 +4,36 @@ function test(x: number, y: number){
 const result = test (1, 2)
 
 console.log(result)
+
+function mul (x: number, y: number):number{
+    return x * y
+}
+
+function div(x: number, y: number):number{
+    return x / y
+}
+
+
+function callfunction(funcs: ((a: number, b: number) => number)[], values: [number, number][]): number[]
+{
+ const results = [] as number[]
+ for (let i = 0; i < funcs.length; i++){
+    const args = values [i]
+    const resul = funcs[i]!(args![0],args![1])
+    results.push(resul)
+ }
+ return results
+} 
+
+console.log((callfunction ([mul,div], [[1,2],[4,5]])))
+
+interface Person{
+    name : string,
+    age :number
+}
+
+const person: Person = {
+    name : "Punam",
+    age : 24
+}
+console.log(person.name)
